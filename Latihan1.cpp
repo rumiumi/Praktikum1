6df49ba6853062da08cba55afd8c1802d32ae7e2
rumiumi <43737118+rumiumi@users.noreply.github.com>
2018-10-02 16:05:41 +0700

    Update Latihan1.cpp

--- Latihan1.cpp
+++ Latihan1.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+  int p, l;
+  
+  cout << "Program menghitung luas persegi panjang"
+  cout << "Masukkan p: "
+  cin >> p
+  cout << "Masukkan l: "
+  cin >> l
+  
+  cout << "Luas = "p*l
+  return 0;
+}

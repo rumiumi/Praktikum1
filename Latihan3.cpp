acf4f3d597471f03c97c3cfefb0242e88c61939b
rumiumi <shintamawarni76@mhs.pelitabangsa.ac.id>
2018-10-09 12:37:28 +0700

    perubahan latihan3

--- Latihan3.cpp
+++ Latihan3.cpp
@@ -4,27 +4,27 @@ using namespace std;
 int main()
 {
   int n1, n2;
-  
-  cout << "Program menentukan angka mana yang lebih besar"
-  cout << "Masukkan bilangan pertama: "
-  cin >> n1
-  cout << "Masukkan bilangan kedua: "
-  cin >> n2
-  
+
+  cout << "[Program menentukan angka mana yang lebih besar]";
+  cout << "Masukkan bilangan pertama: ";
+  cin >> n1;
+  cout << "Masukkan bilangan kedua: ";
+  cin >> n2;
+
   if(n1 > n2)
   {
     cout << n1 << " > " << n2;
   }
-  
+
   if(n1 < n2)
   {
     cout << n1 << " < " << n2;
   }
-  
+
   if(n1 == n2)
   {
     cout << n1 << " = " << n2;
   }
-  
+
   return 0;
 }

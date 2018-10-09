a157d74a14ac545c2a3ada3a1e2b44492a7b91dd
rumiumi <shintamawarni76@mhs.pelitabangsa.ac.id>
2018-10-09 12:44:01 +0700

    perubahan latihan1

--- Latihan1.cpp
+++ Latihan1.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main()
 {
   int p, l;
-  
-  cout << "Program menghitung luas persegi panjang"
-  cout << "Masukkan p: "
-  cin >> p
-  cout << "Masukkan l: "
-  cin >> l
-  
-  cout << "Luas = "p*l
+
+  cout << "[Program menghitung luas persegi panjang] ";
+  cout << "Masukkan p: ";
+  cin >> p;
+  cout << "Masukkan l: ";
+  cin >> l;
+
+  cout << "Luas = ";
+  cout << p * l;
   return 0;
 }
